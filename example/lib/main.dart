56ce66e5ef89485c15d916e2f8f10ce2f516459f
Nolence <rex.magana@thermoworks.com>
2022-07-19 13:22:52 -0600

    added an example

---
+++ example/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:stepper_page_view/stepper_page_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stepper page view example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StepperPageView(
+        pageController: controller,
+        currentPage: 1,
+        pageSteps: const [
+          PageStep(
+            title: Text('Step 1'),
+            content: Center(child: Text('Page 1')),
+          ),
+          PageStep(
+            title: Text('Step 2'),
+            content: Center(child: Text('Page 2')),
+          ),
+          PageStep(
+            title: Text('Step 3'),
+            content: Center(child: Text('Page 3')),
+          ),
+        ],
+      ),
+    );
+  }
+}
